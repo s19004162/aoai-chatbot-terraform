4046a04516b8ab74e15eaa6b2c1b6c2e0d772c8b
TSUYOSHI KIMOTO <70278922+s19004162@users.noreply.github.com>
2023-12-13 22:15:51 +0100

    Update output.tf
    Update output.tf

--- modules/vm/output.tf
+++ modules/vm/output.tf
@@ -3,14 +3,14 @@
 # -----------------------------------------------------
 
 output "vm_id" {
-  value = azurerm_virtual_machine.vm.id
+  value = azurerm_windows_virtual_machine.vm.id
 }
 
 output "vm_name" {
-  value = azurerm_virtual_machine.vm.name
+  value = azurerm_windows_virtual_machine.vm.name
 }
 
 output "vm_private_ip" {
-  value = azurerm_virtual_machine.vm.network_interface_ids[0].private_ip_address
+  value = azurerm_windows_virtual_machine.vm.network_interface_ids[0].private_ip_address
 }
 
